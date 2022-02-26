8490bd5458f1b1d8e13e9b520bdcd6ea1b4073b1
Anh Khoi Do <anhkhoi.do@decathlon.com>
2022-02-26 08:20:37 -0500

    Initial commit

---
+++ app/src/main/java/com/anhkhoido/conversiak/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,45 @@
+package com.anhkhoido.conversiak.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.anhkhoido.conversiak.R
+import com.anhkhoido.conversiak.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+            ViewModelProvider(this).get(GalleryViewModel::class.java)
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
