5890c6b3732fd4b73bed6020cfa62da574082790
Anh Khoi Do <anhkhoi.do@decathlon.com>
2022-02-27 10:58:33 -0500

    Refactoring for conversion fragment

--- app/src/main/java/com/anhkhoido/conversiak/ui/conversion/ConversionFragment.kt
+++ app/src/main/java/com/anhkhoido/conversiak/ui/conversion/ConversionFragment.kt
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.anhkhoido.conversiak.databinding.FragmentHomeBinding
+import com.anhkhoido.conversiak.databinding.FragmentConversionBinding
 
 class ConversionFragment : Fragment() {
 
     private lateinit var conversionViewModel: ConversionViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentConversionBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,7 +27,7 @@ class ConversionFragment : Fragment() {
         conversionViewModel =
             ViewModelProvider(this).get(ConversionViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentConversionBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
