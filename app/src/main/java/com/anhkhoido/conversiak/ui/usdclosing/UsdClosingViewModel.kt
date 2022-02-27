d4c645d507f0b528b73f43d9e0fb6e11356083b1
Anh Khoi Do <anhkhoi.do@decathlon.com>
2022-02-27 11:40:10 -0500

    Change the name of package for fragment about USD's closing value

--- app/src/main/java/com/anhkhoido/conversiak/ui/usdclosing/UsdClosingViewModel.kt
+++ app/src/main/java/com/anhkhoido/conversiak/ui/usdclosing/UsdClosingViewModel.kt
@@ -1,13 +1,13 @@
-package com.anhkhoido.conversiak.ui.gallery
+package com.anhkhoido.conversiak.ui.usdclosing
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class UsdClosingViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is the Fragment for the closing value of the American dollar"
     }
     val text: LiveData<String> = _text
 }
