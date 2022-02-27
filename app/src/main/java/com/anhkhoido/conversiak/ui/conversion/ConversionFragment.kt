1ae14132ab1aafcff556ac333b4b36ebfafa5b03
Anh Khoi Do <anhkhoi.do@decathlon.com>
2022-02-27 10:54:33 -0500

    Change the name from home fragment to conversion

--- app/src/main/java/com/anhkhoido/conversiak/ui/conversion/ConversionFragment.kt
+++ app/src/main/java/com/anhkhoido/conversiak/ui/conversion/ConversionFragment.kt
@@ -1,4 +1,4 @@
-package com.anhkhoido.conversiak.ui.home
+package com.anhkhoido.conversiak.ui.conversion
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,12 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.anhkhoido.conversiak.R
 import com.anhkhoido.conversiak.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class ConversionFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var conversionViewModel: ConversionViewModel
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,14 +24,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        conversionViewModel =
+            ViewModelProvider(this).get(ConversionViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        conversionViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/anhkhoido/conversiak/ui/conversion/ConversionViewModel.kt
+++ app/src/main/java/com/anhkhoido/conversiak/ui/conversion/ConversionViewModel.kt
@@ -1,13 +1,13 @@
-package com.anhkhoido.conversiak.ui.home
+package com.anhkhoido.conversiak.ui.conversion
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ConversionViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is the Fragment for currency conversion"
     }
     val text: LiveData<String> = _text
 }
