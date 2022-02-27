c0ced3e58f55caf81e4ae361b11855596a748e1a
Anh Khoi Do <anhkhoi.do@decathlon.com>
2022-02-27 11:52:56 -0500

    Refactoring the name of about fragment

--- app/src/main/java/com/anhkhoido/conversiak/ui/about/AboutFragment.kt
+++ app/src/main/java/com/anhkhoido/conversiak/ui/about/AboutFragment.kt
@@ -1,4 +1,4 @@
-package com.anhkhoido.conversiak.ui.slideshow
+package com.anhkhoido.conversiak.ui.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.anhkhoido.conversiak.R
-import com.anhkhoido.conversiak.databinding.FragmentSlideshowBinding
+import com.anhkhoido.conversiak.databinding.FragmentAboutBinding
 
-class SlideshowFragment : Fragment() {
+class AboutFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
-    private var _binding: FragmentSlideshowBinding? = null
+    private lateinit var aboutViewModel: AboutViewModel
+    private var _binding: FragmentAboutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        aboutViewModel =
+            ViewModelProvider(this).get(AboutViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+        aboutViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/anhkhoido/conversiak/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/anhkhoido/conversiak/ui/about/AboutViewModel.kt
@@ -1,13 +1,13 @@
-package com.anhkhoido.conversiak.ui.slideshow
+package com.anhkhoido.conversiak.ui.about
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is about Fragment"
     }
     val text: LiveData<String> = _text
 }
