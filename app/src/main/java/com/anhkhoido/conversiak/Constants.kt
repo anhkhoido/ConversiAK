22e07cd8683b77480a10b443d40b4082f64046b5
Anh Khoi Do <anhkhoi.do@decathlon.com>
2022-03-12 19:34:55 -0500

    Creation of constant class

---
+++ app/src/main/java/com/anhkhoido/conversiak/Constants.kt
@@ -0,0 +1,6 @@
+package com.anhkhoido.conversiak
+
+object Constants {
+    const val BANK_OF_CANADA_API_EN = "https://www.bankofcanada.ca/valet"
+    const val BANK_OF_CANADA_API_FR = "https://www.banqueducanada.ca/valet"
+}
